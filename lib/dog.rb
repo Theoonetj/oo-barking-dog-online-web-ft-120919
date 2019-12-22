39357f8635dc7752a2549daee4a83056bedc0dea
confident-token-1019 <theoonetj@gmail.com>
2019-12-22 02:50:33 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,22 @@
 # Your code goes here!
+
+class Dog
+ 
+  def name=(dogs_name)
+    fido = Dog.new
+    fido.dogs_name = "Fido"
+   
+  end 
+  
+  def getter_method(dogs_name)
+     fido.name
+  end 
+  
+  def bark
+    puts "woof!"
+  end
+    
+  end
+  
+  
+  
